770534db051b325a8a32d82fa8829eade0831961
Mouhammed Dbs <mouhammeddbs@gmail.com>
2024-06-27 15:38:33 +0300

    edit

--- helpers.py
+++ helpers.py
@@ -8,9 +8,9 @@ from preprocessing import sentToVec, processSent
 import pickle
 
 # Load models and vocabulary
-model = load_model('models/seq2seq.keras')
-enc_model = load_model('models/encoder_model.keras')
-dec_model = load_model('models/decoder_model.keras')
+# model = load_model('models/seq2seq.keras')
+# enc_model = load_model('models/encoder_model.keras')
+# dec_model = load_model('models/decoder_model.keras')
 disorder_model = load_model('models/disorder_model.keras')
 
 with open('data/vocab.json', 'r') as file:
@@ -39,47 +39,47 @@ def generateResponse(idQues, userRes=None, typeQues='ar'):
             return {"type": "que", "result": random.choice(category_questions.values.tolist())}
         else:
             if predictDisorder(userRes) > 0:
-                return {"type": "seq", "result": generateSeq2Seq(userRes)}
+                return {"type": "seq", "result": "generateSeq2Seq(userRes)"}
             return {"type": "sent", "result": random.choice(normal_res.values.tolist())}
     return {"type": "unknown"}
 
-def generateSeq2Seq(prepro1):
-    tokens = ['<PAD>', '<EOS>', '<OUT>', '<SOS>']
-    max_len = 23
-    prepro = [prepro1]
+# def generateSeq2Seq(prepro1):
+#     tokens = ['<PAD>', '<EOS>', '<OUT>', '<SOS>']
+#     max_len = 23
+#     prepro = [prepro1]
 
-    txt = []
-    for x in prepro:
-        lst = [vocab[word] if word in vocab else vocab['<OUT>'] for word in x.split()]
-        txt.append(lst)
+#     txt = []
+#     for x in prepro:
+#         lst = [vocab[word] if word in vocab else vocab['<OUT>'] for word in x.split()]
+#         txt.append(lst)
 
-    txt = pad_sequences(txt, max_len, padding='post')
-    stat = enc_model.predict(txt)
+#     txt = pad_sequences(txt, max_len, padding='post')
+#     stat = enc_model.predict(txt)
 
-    empty_target_seq = np.zeros((1, 1))
-    empty_target_seq[0, 0] = vocab['<SOS>']
+#     empty_target_seq = np.zeros((1, 1))
+#     empty_target_seq[0, 0] = vocab['<SOS>']
 
-    stop_condition = False
-    decoded_translation = ''
+#     stop_condition = False
+#     decoded_translation = ''
 
-    while not stop_condition:
-        dec_outputs, h, c = dec_model.predict([empty_target_seq] + stat)
-        decoder_concat_input = model.layers[-1](dec_outputs)
-        sampled_word_index = np.argmax(decoder_concat_input[0, -1, :])
-        sampled_word = inv_vocab[str(sampled_word_index)] + ' '
+#     while not stop_condition:
+#         dec_outputs, h, c = dec_model.predict([empty_target_seq] + stat)
+#         decoder_concat_input = model.layers[-1](dec_outputs)
+#         sampled_word_index = np.argmax(decoder_concat_input[0, -1, :])
+#         sampled_word = inv_vocab[str(sampled_word_index)] + ' '
 
-        if sampled_word != '<EOS> ':
-            decoded_translation += sampled_word
+#         if sampled_word != '<EOS> ':
+#             decoded_translation += sampled_word
 
-        if sampled_word == '<EOS> ' or len(decoded_translation.split()) > max_len:
-            stop_condition = True
+#         if sampled_word == '<EOS> ' or len(decoded_translation.split()) > max_len:
+#             stop_condition = True
 
-        empty_target_seq = np.zeros((1, 1))
-        empty_target_seq[0, 0] = sampled_word_index
-        stat = [h, c]
-    for token in tokens:
-        decoded_translation = decoded_translation.replace(token, "")
-    return decoded_translation
+#         empty_target_seq = np.zeros((1, 1))
+#         empty_target_seq[0, 0] = sampled_word_index
+#         stat = [h, c]
+#     for token in tokens:
+#         decoded_translation = decoded_translation.replace(token, "")
+#     return decoded_translation
 
 def predictDisorder(sent):
     padded_sent = pad_sequences(sentToVec([processSent(sent.split(), stopwords)]), maxlen=15, padding='post', dtype='float32')
